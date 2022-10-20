bc9c485c4d1980b73110845fd6406a28074b8ad7
Crambor <dariuscpt@gmail.com>
2022-10-20 15:27:06 +0100

    feat: improve hosts generation

--- generate_hosts.py
+++ generate_hosts.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-import re
+import os
+import json
 
 
-regex = re.compile(r"compute-(\d+).*\s*((?:\d+\.?){4})")
-with open("input.txt", 'r') as f:
-    nodes = {int(node): ip for node, ip in re.findall(regex, f.read())}
+output = os.popen("openstack server list -f json")
+nodes = json.loads(output.read())
+hosts_output = []
 
+for node in nodes:
+    try:
+        ip = node['Networks']['stbl'][0]
+    except KeyError:
+        continue
+    name = '-'.join(f"{substr:>03}" for substr in node['Name'].split('-'))
+    hosts_output += [[name, ip]]
+
+hosts_output.sort(key=lambda x: x[0])
+name, ip = hosts_output[-1]
+output = f"{ip:<12} {name}\n" + "\n".join(f"{ip:<12} {name}" for name, ip in hosts_output[:-1])
 
 with open("src/hosts", 'w') as f:
     f.write("127.0.0.1    localhost localhost.localdomain localhost4 localhost4.localdomain4\n")
     f.write("::1          localhost localhost.localdomain localhost6 localhost6.localdomain6\n")
-    f.write("10.25.32.50  head\n")
-    f.writelines(f"{nodes[i+1]:<12} compute-{i+1:02}\n" for i in range(len(nodes)))
+    f.write(output)
 
 with open("src/hostfile", 'w') as f:
-    f.writelines(f"{nodes[i+1]:<12} slots=20 max_slots=20\n" for i in range(len(nodes)))
+    f.writelines(f"{name} slots=1 max_slots=1\n" for name, ip in hosts_output[:-1])
--- provision-head.sh
+++ provision-head.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+./generate_hosts.py
 ansible-playbook ansible/head-playbook.yaml -i ansible/inventory_hosts
